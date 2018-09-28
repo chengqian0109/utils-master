9d8c074c31cebf41c59e648ea53854c6fca65974
chengqian <chengqian0109@dingtalk.com>
2018-09-28 14:42:28 +0800

    1.修改权限申请工具类PermissionUtils，在该类中增加权限组的定义类Permission 2.可以将权限添加到List中进行权限申请 3.在使用权限组时，可以通过调用PermissionUtils.addPermissions()添加单个或多个权限到集合中统一申请

--- app/src/main/java/com/jack/demo/MainActivity.java
+++ app/src/main/java/com/jack/demo/MainActivity.java
@@ -1,15 +1,62 @@
 package com.jack.demo;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+
+import com.jack.utils.DeviceUtils;
+import com.jack.utils.LogUtils;
+import com.jack.utils.PermissionUtils;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> permissions = PermissionUtils.getPermissions(PermissionUtils.Permission.CALENDAR,
+                PermissionUtils.Permission.PHONE, PermissionUtils.Permission.SMS, PermissionUtils.Permission.STORAGE,
+                PermissionUtils.Permission.CONTACTS);
+
+        PermissionUtils.requestPermissions(this, new PermissionUtils.PermissionCallback() {
+            @Override
+            public void onAllPermissionAllowed() {
+                LogUtils.e("onAllPermissionAllowed：1 = " + checkSelfPermission(Manifest.permission.BLUETOOTH));
+                LogUtils.e("onAllPermissionAllowed：2 = " + checkSelfPermission(Manifest.permission.CAMERA));
+                LogUtils.e("onAllPermissionAllowed：3 = " + checkSelfPermission(Manifest.permission.READ_CALENDAR));
+                LogUtils.e("onAllPermissionAllowed：4 = " + checkSelfPermission(Manifest.permission.READ_SMS));
+                LogUtils.e("onAllPermissionAllowed：5 = " + checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE));
+                LogUtils.e("onAllPermissionAllowed：6 = " + DeviceUtils.getPhoneNumber(MainActivity.this));
+                LogUtils.e("onAllPermissionAllowed：7 = " + checkSelfPermission(Manifest.permission.WRITE_CONTACTS));
+            }
+
+            @Override
+            public void onRequestPermissionAgain() {
+                LogUtils.e("onRequestPermissionAgain：1 = " + checkSelfPermission(Manifest.permission.BLUETOOTH));
+                LogUtils.e("onRequestPermissionAgain：2 = " + checkSelfPermission(Manifest.permission.CAMERA));
+                LogUtils.e("onRequestPermissionAgain：3 = " + checkSelfPermission(Manifest.permission.READ_CALENDAR));
+                LogUtils.e("onRequestPermissionAgain：4 = " + checkSelfPermission(Manifest.permission.READ_SMS));
+                LogUtils.e("onRequestPermissionAgain：5 = " + checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE));
+                LogUtils.e("onRequestPermissionAgain：6 = " + DeviceUtils.getPhoneNumber(MainActivity.this));
+                LogUtils.e("onRequestPermissionAgain：7 = " + checkSelfPermission(Manifest.permission.WRITE_CONTACTS));
+            }
+
+            @Override
+            public void onGuideUserToSettings() {
+                LogUtils.e("onGuideUserToSettings：1 = " + checkSelfPermission(Manifest.permission.BLUETOOTH));
+                LogUtils.e("onGuideUserToSettings：2 = " + checkSelfPermission(Manifest.permission.CAMERA));
+                LogUtils.e("onGuideUserToSettings：3 = " + checkSelfPermission(Manifest.permission.READ_CALENDAR));
+                LogUtils.e("onGuideUserToSettings：4 = " + checkSelfPermission(Manifest.permission.READ_SMS));
+                LogUtils.e("onGuideUserToSettings：5 = " + checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE));
+                LogUtils.e("onGuideUserToSettings：6 = " + DeviceUtils.getPhoneNumber(MainActivity.this));
+                LogUtils.e("onGuideUserToSettings：7 = " + checkSelfPermission(Manifest.permission.WRITE_CONTACTS));
+            }
+        }, PermissionUtils.addPermissions(permissions, Manifest.permission.BLUETOOTH, Manifest.permission.CAMERA));
     }
 }
--- utils/src/main/java/com/jack/utils/PermissionUtils.java
+++ utils/src/main/java/com/jack/utils/PermissionUtils.java
@@ -1,10 +1,18 @@
 package com.jack.utils;
 
+import android.Manifest;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.annotations.Nullable;
 import io.reactivex.disposables.Disposable;
 
 /**
@@ -14,6 +22,63 @@ import io.reactivex.disposables.Disposable;
  * Created on 2018/07/23
  */
 public class PermissionUtils {
+    /**
+     * 权限，包含一部分组权限，其他单个权限请使用 {@link Manifest.permission}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public static final class Permission {
+        /**
+         * 存储权限
+         */
+        public static final String[] STORAGE = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+        /**
+         * 位置权限
+         */
+        public static final String[] LOCATION = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+        /**
+         * 电话权限
+         */
+        public static final String[] PHONE = {
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.WRITE_CALL_LOG,
+                Manifest.permission.USE_SIP,
+                Manifest.permission.PROCESS_OUTGOING_CALLS
+        };
+        /**
+         * 日历权限
+         */
+        public static final String[] CALENDAR = {
+                Manifest.permission.READ_CALENDAR,
+                Manifest.permission.WRITE_CALENDAR
+        };
+        /**
+         * 联系人权限
+         */
+        public static final String[] CONTACTS = {
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.WRITE_CONTACTS,
+                Manifest.permission.GET_ACCOUNTS
+        };
+        /**
+         * 短信权限
+         */
+        public static final String[] SMS = {
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.RECEIVE_SMS,
+                Manifest.permission.READ_SMS,
+                Manifest.permission.RECEIVE_WAP_PUSH,
+                Manifest.permission.RECEIVE_MMS
+        };
+    }
+
     /**
      * 被禁止后可以显示权限申请弹窗的权限个数
      */
@@ -28,6 +93,54 @@ public class PermissionUtils {
      * 勾选不再询问的权限的请求个数
      */
     private static int sDoNotAskAgainPermissionCount = 0;
+
+    /**
+     * 请求权限
+     *
+     * @param activity       必须是FragmentActivity
+     * @param permissionList 请求的权限集合
+     */
+    public static Disposable requestPermissions(FragmentActivity activity, PermissionCallback callback, List<String> permissionList) {
+        if (activity != null && callback != null && permissionList != null && permissionList.size() != 0) {
+            // 再次进行权限申请操作时清零
+            sAllowedPermissionCount = 0;
+            sShouldShowRequestPermissionRationaleCount = 0;
+            sDoNotAskAgainPermissionCount = 0;
+            String[] permissions = new String[permissionList.size()];
+            for (int i = 0; i < permissionList.size(); i++) {
+                permissions[i] = permissionList.get(i);
+            }
+            RxPermissions rxPermissions = new RxPermissions(activity);
+            return rxPermissions.requestEach(permissions).subscribe(permission -> {
+                // 会发出指定权限个数的订阅对象
+                if (permission.granted) {
+                    sAllowedPermissionCount += 1;
+                    // 权限已允许
+                } else if (permission.shouldShowRequestPermissionRationale) {
+                    // 权限被禁止，但没有勾选不再询问
+                    sShouldShowRequestPermissionRationaleCount += 1;
+                } else {
+                    // 勾选了不再询问，需要引导用户到应用详情去设置
+                    sDoNotAskAgainPermissionCount += 1;
+                }
+                // 所有权限已申请完毕才进行相关弹窗操作
+                if (sAllowedPermissionCount + sShouldShowRequestPermissionRationaleCount
+                        + sDoNotAskAgainPermissionCount == permissions.length) {
+                    if (sDoNotAskAgainPermissionCount != 0) {
+                        // 至少一个勾选了不再询问，弹窗去设置
+                        callback.onGuideUserToSettings();
+                    } else if (sShouldShowRequestPermissionRationaleCount != 0) {
+                        // 已禁止的权限都没有勾选不再询问，弹窗重新提醒申请
+                        callback.onRequestPermissionAgain();
+                    } else {
+                        // 权限已全部允许
+                        callback.onAllPermissionAllowed();
+                    }
+                }
+            });
+        }
+        return null;
+    }
 
     /**
      * 请求权限
@@ -59,19 +172,13 @@ public class PermissionUtils {
                         + sDoNotAskAgainPermissionCount == permissions.length) {
                     if (sDoNotAskAgainPermissionCount != 0) {
                         // 至少一个勾选了不再询问，弹窗去设置
-                        if (callback != null) {
-                            callback.onGuideUserToSettings();
-                        }
+                        callback.onGuideUserToSettings();
                     } else if (sShouldShowRequestPermissionRationaleCount != 0) {
                         // 已禁止的权限都没有勾选不再询问，弹窗重新提醒申请
-                        if (callback != null) {
-                            callback.onRequestPermissionAgain();
-                        }
+                        callback.onRequestPermissionAgain();
                     } else {
                         // 权限已全部允许
-                        if (callback != null) {
-                            callback.onAllPermissionAllowed();
-                        }
+                        callback.onAllPermissionAllowed();
                     }
                 }
             });
@@ -86,7 +193,7 @@ public class PermissionUtils {
      * @param permissions 请求的权限(个数可变)
      */
     public static Disposable requestPermissions(Fragment fragment, PermissionCallback callback, String... permissions) {
-        if (permissions != null && permissions.length != 0) {
+        if (fragment != null && callback != null && permissions != null && permissions.length != 0) {
             // 再次进行权限申请操作时清零
             sAllowedPermissionCount = 0;
             sShouldShowRequestPermissionRationaleCount = 0;
@@ -109,24 +216,99 @@ public class PermissionUtils {
                         + sDoNotAskAgainPermissionCount == permissions.length) {
                     if (sDoNotAskAgainPermissionCount != 0) {
                         // 至少一个勾选了不再询问，弹窗去设置
-                        if (callback != null) {
-                            callback.onGuideUserToSettings();
-                        }
+                        callback.onGuideUserToSettings();
                     } else if (sShouldShowRequestPermissionRationaleCount != 0) {
                         // 已禁止的权限都没有勾选不再询问，弹窗重新提醒申请
-                        if (callback != null) {
-                            callback.onRequestPermissionAgain();
-                        }
+                        callback.onRequestPermissionAgain();
                     } else {
                         // 权限已全部允许
-                        if (callback != null) {
-                            callback.onAllPermissionAllowed();
-                        }
+                        callback.onAllPermissionAllowed();
                     }
                 }
             });
         }
         return null;
+    }
+
+    /**
+     * 请求权限
+     *
+     * @param fragment       必须是v4包下的Fragment
+     * @param permissionList 请求的权限集合
+     */
+    public static Disposable requestPermissions(Fragment fragment, PermissionCallback callback, List<String> permissionList) {
+        if (fragment != null && callback != null && permissionList != null && permissionList.size() > 0) {
+            // 再次进行权限申请操作时清零
+            sAllowedPermissionCount = 0;
+            sShouldShowRequestPermissionRationaleCount = 0;
+            sDoNotAskAgainPermissionCount = 0;
+            String[] permissions = new String[permissionList.size()];
+            for (int i = 0; i < permissionList.size(); i++) {
+                permissions[i] = permissionList.get(i);
+            }
+            RxPermissions rxPermissions = new RxPermissions(fragment);
+            return rxPermissions.requestEach(permissions).subscribe(permission -> {
+                // 会发出指定权限个数的对象
+                if (permission.granted) {
+                    sAllowedPermissionCount += 1;
+                    // 权限已允许
+                } else if (permission.shouldShowRequestPermissionRationale) {
+                    // 权限被禁止，但没有勾选不再询问
+                    sShouldShowRequestPermissionRationaleCount += 1;
+                } else {
+                    // 勾选了不再询问，需要引导用户到应用详情去设置
+                    sDoNotAskAgainPermissionCount += 1;
+                }
+                // 所有权限已申请完毕才进行相关弹窗操作
+                if (sAllowedPermissionCount + sShouldShowRequestPermissionRationaleCount
+                        + sDoNotAskAgainPermissionCount == permissions.length) {
+                    if (sDoNotAskAgainPermissionCount != 0) {
+                        // 至少一个勾选了不再询问，弹窗去设置
+                        callback.onGuideUserToSettings();
+                    } else if (sShouldShowRequestPermissionRationaleCount != 0) {
+                        // 已禁止的权限都没有勾选不再询问，弹窗重新提醒申请
+                        callback.onRequestPermissionAgain();
+                    } else {
+                        // 权限已全部允许
+                        callback.onAllPermissionAllowed();
+                    }
+                }
+            });
+        }
+        return null;
+    }
+
+    /**
+     * 根据本类中的 {@link Permission} 定义的权限组将权限进行整合
+     *
+     * @param permissionGroup 权限组 {@link Permission}
+     * @return 要申请的权限集合
+     */
+    public static List<String> getPermissions(String[]... permissionGroup) {
+        List<String> permissionList = new ArrayList<>();
+        if (permissionGroup != null && permissionGroup.length != 0) {
+            for (String[] arrays : permissionGroup) {
+                permissionList.addAll(Arrays.asList(arrays));
+            }
+        }
+        return permissionList;
+    }
+
+    /**
+     * 添加指定权限到指定集合
+     *
+     * @param permissionList 权限要存储的集合
+     * @param permissions    要添加的权限（可变参数）
+     * @return 所有要申请的权限集合
+     */
+    public static List<String> addPermissions(@Nullable List<String> permissionList, String... permissions) {
+        permissionList = permissionList == null ? new ArrayList<>() : permissionList;
+        if (permissions != null && permissions.length != 0) {
+            for (String permission : permissions) {
+                permissionList.add(permission);
+            }
+        }
+        return permissionList;
     }
 
     /**
